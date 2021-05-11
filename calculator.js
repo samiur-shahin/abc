85a33fb5094ded1ce6cc12f5241d9e74efc2c533
samiur-shahin <83329089+samiur-shahin@users.noreply.github.com>
2021-05-11 21:03:30 +0600

    zjzjs

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function off(){
+
+document.getElementByld("one").disabled=true;
+document.getElementByld("two").disabled=true;
+document.getElementByld("there").disabled=true;
+document.getElementByld("1").disabled=true;
+document.getElementByld("2").disabled=true;
+document.getElementByld("3").disabled=true;
+document.getElementByld("4").disabled=true;
+document.getElementByld("5").disabled=true;
+document.getElementByld("6").disabled=true;
+document.getElementByld("7").disabled=true;
+document.getElementByld("8").disabled=true;
+document.getElementByld("9").disabled=true;
+document.getElementByld("0").disabled=true;
+document.getElementByld("+").disabled=true;
+document.getElementByld("/").disabled=true;
+document.getElementByld("-").disabled=true;
+document.getElementByld("%").disabled=true;
+document.getElementByld("*").disabled=true;
+
+}
